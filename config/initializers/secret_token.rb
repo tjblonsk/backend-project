231f9b5f680d3dc8ad92d8bbdf053343174ab62d
Tim <tim.blonski@gmail.com>
2014-06-01 16:31:06 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pizzeria::Application.config.secret_key_base = '6137c238de6d7b2fae7fd035504c75e526d70b29e4697a3923f10d14e4b35c2d9f85bf40691c219a230d263afe1a2a8523ec3d58585e240b113d6b528beb1309'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pizzeria::Application.config.session_store :cookie_store, key: '_Pizzeria_session'
